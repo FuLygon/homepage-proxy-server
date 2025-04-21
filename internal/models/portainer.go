5d863da9a9b08d2f3edf3221e9f79c4b7ef55f3f
Thanh Thai <thanhthai@fulygon.com>
2025-04-22 00:32:46 +0700

    Update Portainer models
    Signed-off-by: Thanh Thai <thanhthai@fulygon.com>

--- internal/models/portainer.go
+++ internal/models/portainer.go
@@ -1,10 +1,12 @@
 package models
 
+// PortainerStats lists of containers and stats
 type PortainerStats struct {
 	State  string `json:"State"`
 	Status string `json:"Status"`
 }
 
+// PortainerResponse minimalized response from Portainer API
 type PortainerResponse struct {
 	Total   int `json:"total"`
 	Running int `json:"running"`
