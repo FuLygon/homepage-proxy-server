97c7f22fa2de6c887bdaa9cc1a262ad3001b3183
Thanh Thai <thanhthai@fulygon.com>
2025-04-22 00:33:23 +0700

    Update WUD models
    Signed-off-by: Thanh Thai <thanhthai@fulygon.com>

--- internal/models/wud.go
+++ internal/models/wud.go
@@ -1,9 +1,11 @@
 package models
 
+// WUDStats lists available container with update
 type WUDStats struct {
 	UpdateAvailable bool `json:"updateAvailable"`
 }
 
+// WUDResponse minimalized response from WUD API
 type WUDResponse struct {
 	Monitoring int `json:"monitoring"`
 	Updates    int `json:"updates"`
