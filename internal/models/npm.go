311fab3e3d96e6b8330e2e66c68d2d8f30dd18a9
Thanh Thai <thanhthai@fulygon.com>
2025-04-22 00:31:46 +0700

    Update NPM models
    Signed-off-by: Thanh Thai <thanhthai@fulygon.com>

--- internal/models/npm.go
+++ internal/models/npm.go
@@ -2,22 +2,26 @@ package models
 
 import "time"
 
+// NpmAuthRequest authentication request
 type NpmAuthRequest struct {
 	Identity string `json:"identity"`
 	Secret   string `json:"secret"`
 }
 
+// NpmAuthResponse authentication response
 type NpmAuthResponse struct {
 	Token   string    `json:"token"`
 	Expires time.Time `json:"expires"`
 }
+
+// NPMProxyHostsStats lists of proxy hosts
 type NPMProxyHostsStats struct {
 	ID          int      `json:"id"`
 	Enabled     bool     `json:"enabled"`
 	DomainNames []string `json:"domain_names"`
 }
 
-// NPMResponse holds the statistics about proxy hosts
+// NPMResponse minimalized response from NPM API
 type NPMResponse struct {
 	Total    int `json:"total"`
 	Enabled  int `json:"enabled"`
